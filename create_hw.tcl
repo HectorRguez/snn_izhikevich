a85a300ad799a7b2f54f2cfb78fa249644afee1e
pt2690845 <hectorrodriguezrodriguez52@gmail.com>
2024-02-23 12:27:31 +0100

    Corrected locked IP problem

--- create_hw.tcl
+++ create_hw.tcl
@@ -1,14 +1,20 @@
 # Configure the board
 set_part xc7z020clg400-1
 
+# Add Custom IP to the project IP catalog
+set_property ip_repo_paths hls_snn [current_fileset]
+update_ip_catalog
+
 # Read the previously created block design
 read_bd block_design/block_design.bd
+report_ip_status
 
 # Create the wrapper
-make_wrapper -top -files [get_files block_design/block_design.bd] -fileset . -force
+make_wrapper -top -files [get_files block_design.bd] -force
+read_verilog block_design/hdl/block_design_wrapper.v
 
 #If the block design does not have the output products generated, generate the output products needed for synthesis and implementation runs
-generate_target all [get_files block_design.bd]
+generate_target all [get_files block_design/block_design.bd]
 
 #Run synthesis and implementation
 synth_design -top block_design_wrapper
