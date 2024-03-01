73aaa7bdc6440377a1ca1b91dfda5e4481b0d9a5
pt2690845 <hectorrodriguezrodriguez52@gmail.com>
2024-03-01 13:48:47 +0100

    Added change directory on the Vivado scripts

--- vivado/export_hw.tcl
+++ vivado/export_hw.tcl
@@ -1,8 +1,11 @@
+# Move to the working directory for vitis_hls
+cd vivado/
+
 # Read routed design
-open_checkpoint vivado/checkpoints/route.dcp
+open_checkpoint checkpoints/route.dcp
 
 # Save bitstream separately
-write_bitstream -force vivado/snn_hw.bit
+write_bitstream -force snn_hw.bit
 
 #Export the implemented hardware system to the Vitis environment
-write_hw_platform -fixed -force -include_bit -file vivado/snn_hw.xsa
+write_hw_platform -fixed -force -include_bit -file snn_hw.xsa
--- vivado/place_and_route.tcl
+++ vivado/place_and_route.tcl
@@ -1,12 +1,15 @@
+# Move to the working directory for vitis_hls
+cd vivado/
+
 # Read synthezised design
-open_checkpoint vivado/checkpoints/synth.dcp
+open_checkpoint checkpoints/synth.dcp
 
 # Placing Design
 place_design
-write_checkpoint -force vivado/checkpoints/place.dcp
+write_checkpoint -force checkpoints/place.dcp
 
 # Routing Design
 route_design
 
 # Saving Run
-write_checkpoint -force vivado/checkpoints/route.dcp
+write_checkpoint -force checkpoints/route.dcp
