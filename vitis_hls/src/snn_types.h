81e6ee77255c0b3d6ecf774347da6800acc4b1f4
hector <he.rodriguez@alumnos.upm.es>
2024-03-08 15:53:40 +0100

    Update todo list

--- vitis_hls/src/snn_types.h
+++ vitis_hls/src/snn_types.h
@@ -17,15 +17,6 @@
 /*****************************************************************************/
 /*                            Type's Definitions                             */
 /*****************************************************************************/
-// Fixed sizes
-/*#define VU_WIDTH		32
-#define VU_INT			10
-#define VU_FRAC 		(VU_WIDTH - VU_INT - 1)
-#define S_WIDTH			16
-#define S_INT			5
-#define S_FRAC 			(S_WIDTH - S_INT)*/
-
-// HW version
 #if PRECISION_TYPE == FIXED_POINT
 typedef ap_ufixed<WEIGHT_BITS, 0, AP_TRN, AP_WRAP> w_dat_t;
 typedef ap_ufixed<SYNAPSE_BITS, SYNAPSE_BITS_INT, AP_TRN, AP_WRAP> s_dat_t;
@@ -42,16 +33,6 @@ typedef ap_uint<3>			uint3_t;
 typedef float 				float32_t;
 typedef short 				int16_t;
 typedef unsigned short 		uint16_t;
-#ifdef DEFINE_INT32
-typedef int 				int32_t;
-typedef unsigned int 		uint32_t;
-#endif
-
-// SW version
-typedef char uint1_sw_t;
-typedef float w_dat_sw_t;
-typedef float s_dat_sw_t;
-typedef float vu_dat_sw_t;
 
 /*****************************************************************************/
 /*                            Classes and Utilities                          */
@@ -105,11 +86,5 @@ INLINE uint64_t uint32_to_uint64(uint32_t value1, uint32_t value2) {
 	c.u32data2 = value2;
 	return c.u64data;
 }
-
-#if PRECISION_TYPE == FIXED_POINT
-#define norm_weight(weight)	(weight == 0 ? 0 : ((weight < 0.0004883? 0.0004883 : weight) * WEIGHT_SCALE))
-#elif PRECISION_TYPE == FLOATING_POINT
-#define norm_weight(weight)	(weight)
-#endif /* PRECISION_TYPE */
 
 #endif /* _SNN_TYPES_H_ */
