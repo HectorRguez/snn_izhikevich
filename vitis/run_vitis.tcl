8476b19a374f6dfe526dee17d5b26053caf819aa
HectorRguez <he.rodriguez@alumnos.upm.es>
2024-07-26 14:27:30 +0200

    Checked run_hls

--- vitis/run_vitis.tcl
+++ vitis/run_vitis.tcl
@@ -3,18 +3,19 @@ connect -url tcp:127.0.0.1:3121
 targets -set -nocase -filter {name =~"APU*"}
 rst -system
 after 3000
-targets -set -filter {jtag_cable_name =~ "Xilinx PYNQ-Z1 003017A6DC79A" && level==0 && jtag_device_ctx=="jsn-Xilinx PYNQ-Z1-003017A6DC79A-23727093-0"}
-fpga -file vitis/ws/snn_izhikevich_app/_ide/bitstream/snn_hw.bit
+targets -set -filter {jtag_cable_name =~ "Xilinx PYNQ-Z1 003017A6E1A7A" && level==0 && jtag_device_ctx=="jsn-Xilinx PYNQ-Z1-003017A6E1A7A-23727093-0"}
+fpga -file /home/hector/Desktop/TFM/HW-SW_Platform/Automatic_platform/accelerator_impl/vitis/ws/snn_izhikevich_app/_ide/bitstream/snn_hw.bit
 targets -set -nocase -filter {name =~"APU*"}
-loadhw -hw vitis/ws/snn_izhikevich_pl/export/snn_izhikevich_pl/hw/snn_hw.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+loadhw -hw /home/hector/Desktop/TFM/HW-SW_Platform/Automatic_platform/accelerator_impl/vitis/ws/snn_izhikevich_pl/export/snn_izhikevich_pl/hw/snn_hw.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~"APU*"}
-source vitis/ws/snn_izhikevich_app/_ide/psinit/ps7_init.tcl
+source /home/hector/Desktop/TFM/HW-SW_Platform/Automatic_platform/accelerator_impl/vitis/ws/snn_izhikevich_app/_ide/psinit/ps7_init.tcl
 ps7_init
 ps7_post_config
 targets -set -nocase -filter {name =~ "*A9*#0"}
-dow vitis/ws/snn_izhikevich_app/Debug/snn_izhikevich_app.elf
+dow /home/hector/Desktop/TFM/HW-SW_Platform/Automatic_platform/accelerator_impl/vitis/ws/snn_izhikevich_app/Debug/snn_izhikevich_app.elf
 configparams force-mem-access 0
 
 # Connect
 con
+
